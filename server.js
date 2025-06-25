be0067ba9b4c6c0a6a5e6ee5887fd3f5e2d70eb8
FLORENTIN BOUX <41517731-bouxflorentin@users.noreply.replit.com>
2025-06-25 16:16:48 +0000

    Fix PORT fallback

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { db } from './firebase.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
